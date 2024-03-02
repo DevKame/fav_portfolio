2a31d6d578e45cec7e89658636e96932a0ce7ea1
Kamedin <dev@dev.dev>
2024-03-02 18:51:51 +0100

    progress bar works when scrolling

--- src/App.vue
+++ src/App.vue
@@ -1,12 +1,38 @@
 <template>
+  <the-bar></the-bar>
   <the-header></the-header>
+  <section></section>
 </template>
 
 <script lang="ts" setup>
 import TheHeader from "./components/singleUse/TheHeader.vue";
+import TheBar from "./components/singleUse/TheBar.vue";
+
+import { onMounted, ref, watch, computed, provide } from "vue";
+
+// REFERENCE TO THE <body> ELEMENT
+const body: HTMLBodyElement = document.querySelector("body");
+// REFERENCE TO THE CURRENT scrollY - VALUE
+const sval = ref<number>(scrollY);
+// COMPUTES THE SCROLLPROGRESS OF THE DOCUMENT IN PERCENT
+const scrollProgress = computed(() => {
+  const max = body.getBoundingClientRect().height - innerHeight;
+  return (sval.value / max) * 100;
+});
+//PROVIDING THE SCROLL PROGRESS FOR "FADING IN" OF OTHER COMPS
+provide("scrollprogress", scrollProgress);
+// SET A SCROLL LISTENER TO THE WINDOW ELEMENT
+onMounted(() => {
+  window.addEventListener("scroll", e => {
+    sval.value = scrollY;
+  });
+})
 </script>
 
 <style>
+section {
+  height: 400vh;
+}
 /* section {
   width: 100%;
   height: 100vh;
---
+++ src/components/singleUse/TheBar.vue
@@ -0,0 +1,35 @@
+<template>
+    <aside>
+        <div class="bar bg-tert" ref="bar"></div>
+    </aside>
+</template>
+
+<script lang="ts" setup>
+import {inject, watch, ref} from "vue";
+
+// REFERENCE TO THE <div> ELEMENT DISPLAYING THE CURRRENT SCROLLPROGRESS IN %
+const bar = ref<HTMLDivElement>();
+// INJECTED SCROLL PROGRESS OF THE DOCUMENT IN PERCENT
+let progress = inject("scrollprogress");
+// CHANGE div.bar WITH WHEN SCROLLPROGRESS CHANGES
+watch(progress, val => {
+    let percent = progress.value;
+    if(percent > 100) percent = 100;
+    bar.value.style.width = val + "%";
+})
+</script>
+
+<style scoped>
+aside {
+    z-index: 2;
+    position: fixed;
+    top: 0;
+    left: 0;
+    width: 100%;
+    height: 5px;
+}
+.bar {
+    width: 0;
+    height: 100%;
+}
+</style>
